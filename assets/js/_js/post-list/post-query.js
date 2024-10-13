26472b4170cc4b463da7dcb7ea2b44b4b85182dc
lkxnif <1103190704@qq.com>
2024-10-14 00:09:01 +0800

    tags_archive fix

--- assets/js/_js/post-list/post-query.js
+++ assets/js/_js/post-list/post-query.js
@@ -135,20 +135,18 @@
   function filterQuery(posts, property, value) {
     let queryResult = [];
     for (let post of posts) {
-      // 排除 archive 为 true 的帖子
-      if (post.archive === true || post.archive === 'true') {
+      // 排除 archive 为 true 的帖子或没有指定属性的帖子
+      if (post.archive === true || post.archive === 'true' || typeof post[property] === 'undefined') {
         continue;
       }
 
-      /* 如果帖子没有指定的属性，跳过 */
-      if (typeof post[property] === 'undefined') continue;
       let prop = post[property].split(", ");
-      /* 如果属性值为空，跳过 */
+      /* if it doesn't have any item, pass it */
       if (prop[0] == '') continue;
       for (let item of prop) {
         if (machValue(item, value) == true) {
           queryResult.push(post);
-          /* 防止重复，添加一次后跳出 */
+          /* prevent duplicates, add post once */
           break;
         }
       }
